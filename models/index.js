9addb8ef68df5ff38a845b1ca0dff465b4088a5a
Daniel Barclay <daniel@urbanintel.io>
2018-10-17 22:35:25 +0100

    Fixed underscore issue in Index model

--- models/index.js
+++ models/index.js
@@ -1,21 +1,23 @@
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'daniel', '', {
-    dialect: 'postgres',
+  dialect: 'postgres',
+  define: {
     underscored: true,
+  },
 });
 
 const models = {
-    User: sequelize.import('./user'),
-    Channel: sequelize.import('./channel'),
-    Message: sequelize.import('./message'),
-    Team: sequelize.import('./team'),
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
 };
 
 Object.keys(models).forEach((modelName) => {
-    if ('associate' in models[modelName]) {
-        models[modelName].associate(models);
-    }
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
 });
 
 models.sequelize = sequelize;
