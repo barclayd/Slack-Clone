edad280a06f8e3f4d11e4e1e15bbceb9a6b519b8
Daniel Barclay <barclaysd@me.com>
2019-04-21 19:01:08 +0100

    Fixed bug to allow names to be stored in Postgres as snake_case instead of camelCase. Updated schema to support new models

--- models/message.js
+++ models/message.js
@@ -1,6 +1,8 @@
 export default (sequelize, DataTypes) => {
   const Message = sequelize.define('message', {
     text: DataTypes.STRING,
+  }, {
+    underscored: true,
   });
 
   Message.associate = (models) => {
