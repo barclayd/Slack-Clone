fa95625257bec080693590717df50402beb7a1b0
Daniel Barclay <barclaysd@me.com>
2019-04-21 18:33:06 +0100

    Added Sequelize to support Postgres models and establish relations between tables

---
+++ models/channel.js
@@ -0,0 +1,28 @@
+
+export default (sequelize, DataTypes) => {
+  const Channel = sequelize.define(
+    'channel', {
+      name: DataTypes.STRING,
+      public: DataTypes.BOOLEAN,
+    },
+  );
+
+  Channel.associate = (models) => {
+    // 1:M
+    Channel.belongsTo(models.Team, {
+      foreignKey: {
+        name: 'teamId',
+        field: 'team_id',
+      },
+    });
+    Channel.belongsToMany(models.User, {
+      through: 'channel_member',
+      foreignKey: {
+        name: 'channelID',
+        field: 'channel_id',
+      },
+    });
+  };
+
+  return Channel;
+};
--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,5 @@
 export default {
   Query: {
-    hi: () => 'hi',
+    hi: () => 'Hello World',
   },
 };
