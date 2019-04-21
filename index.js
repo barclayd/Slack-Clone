166825a45ecd0b511602a6e93ef45431e958bfd6
Daniel Barclay <barclaysd@me.com>
2019-04-21 13:38:17 +0100

    Cleaned up code base, refactored server to use GraphQL playground

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-import path from 'path';
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-import models from './models';
-
-const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
-
-const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
-
 const express = require('express');
-const {
-  ApolloServer,
-  gql,
-} = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
+server.applyMiddleware({ app });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-server.applyMiddleware({
-  app,
-});
-
-models.sequelize.sync({
-  force: true,
-}).then(() => {
-  app.listen({
-    port: PORT,
-  }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
-});
+app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
