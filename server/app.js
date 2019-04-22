3542ff66eb40b1cbc565ba277be28346a1dfd991
Daniel Barclay <daniel@urbanintel.io>
2019-04-22 20:33:51 +0100

    Added routing, connected frontend to retrieve data from server using Apollo Client and added support for CORS on server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
+import cors from 'cors';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
 import models from './models';
+
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
@@ -17,8 +19,10 @@ const server = new ApolloServer({
 });
 
 const app = express();
+app.use(cors('*'));
+
 server.applyMiddleware({ app });
 // sync models with postgres before running server
 models.sequelize.sync().then(() => {
-  app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+  app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)); // eslint-disable-line no-console
 });
