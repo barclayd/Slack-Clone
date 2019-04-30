548ee0177bd78f64c65ce9f7f58c2ee9e2ef5217
Daniel Barclay <barclaysd@me.com>
2019-04-30 01:16:56 +0100

    Added middleware on client and server to send both token and refreshToken to identify user and authenticate requests dynamically

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
 import 'semantic-ui-css/semantic.min.css';
 
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/graphql',
+//   request: (operation) => {
+//     operation.setContext({
+//       headers: {
+//         token: localStorage.getItem('token'),
+//         'refresh-token': localStorage.getItem('refreshToken'),
+//       },
+//     });
+//   },
+// });
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token || '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
 });
 
 const App = (
