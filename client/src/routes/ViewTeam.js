00d25ebb5a6b849a1cbff32b31c229d3e551dcf1
Daniel Barclay <barclaysd@me.com>
2019-05-02 13:34:40 +0100

    Added base for components to model Slack sidebar, header and message input field

---
+++ client/src/routes/ViewTeam.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import TeamSidebar from '../components/TeamSidebar';
+import TeamHeader from '../components/TeamHeader';
+import MessageInput from '../components/MessageInput';
+
+
+export default () => (
+  <>
+    <TeamHeader />
+    <TeamSidebar />
+    <MessageInput />
+  </>
+);
