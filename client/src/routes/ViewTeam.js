c9758896c1a498a6ac03c3799cb9b676fa60aad9
Daniel Barclay <barclaysd@me.com>
2019-05-04 00:01:01 +0100

    Improved layout and themed it to slack's colour scheme

--- client/src/routes/ViewTeam.js
+++ client/src/routes/ViewTeam.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
 
 import TeamSidebar from '../components/TeamSidebar';
 import TeamHeader from '../components/TeamHeader';
 import MessageInput from '../components/MessageInput';
 
 export default () => (
-  <>
+  <Container>
     <Grid>
       <Grid.Column width={5}>
         <TeamSidebar
@@ -16,10 +16,10 @@ export default () => (
           usersToDirectMessage={['Slackbot', 'Dan', 'Kingsley']}
         />
       </Grid.Column>
-      <Grid.Column width={5}>
+      <Grid.Column width={10}>
         <TeamHeader />
         <MessageInput />
       </Grid.Column>
     </Grid>
-  </>
+  </Container>
 );
