dcbd316b57b046ef8d8d060c30d94763939a3f7f
Daniel Barclay <barclaysd@me.com>
2019-05-03 23:49:41 +0100

    Refactored team sidebar to use custom made grid

--- client/src/routes/ViewTeam.js
+++ client/src/routes/ViewTeam.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 import TeamSidebar from '../components/TeamSidebar';
 import TeamHeader from '../components/TeamHeader';
 import MessageInput from '../components/MessageInput';
 
-
 export default () => (
   <>
-    <TeamHeader />
-    <TeamSidebar />
-    <MessageInput />
+    <Grid>
+      <Grid.Column width={5}>
+        <TeamSidebar
+          teamName="Team Cardiff"
+          userName="Dan"
+          channelNames={['General', 'Random']}
+          usersToDirectMessage={['Slackbot', 'Dan', 'Kingsley']}
+        />
+      </Grid.Column>
+      <Grid.Column width={5}>
+        <TeamHeader />
+        <MessageInput />
+      </Grid.Column>
+    </Grid>
   </>
 );
