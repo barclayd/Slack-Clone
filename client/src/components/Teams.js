e442011db2504ed1779a2c85c1fb6151c1eebd76
Daniel Barclay <barclaysd@me.com>
2019-05-11 00:26:43 +0100

    Added props for dynamic content display for Channels and Teams side bars

---
+++ client/src/components/Teams.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TeamWrapper = styled.div`
+  grid-column: 1;
+  grid-row: 1 / 4;
+  background-color: #362333;
+  color: #958993
+`;
+
+const team = ({ id, letter }) => <li key={`team-${id}`}>{letter}</li>;
+
+export default ({ teams }) => (
+  <TeamWrapper>
+    <ul>
+      <li>Teams</li>
+      {teams.map(team)}
+    </ul>
+  </TeamWrapper>
+);
