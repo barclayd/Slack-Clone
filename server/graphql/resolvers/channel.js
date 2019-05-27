fb529097be1167683df0ce659c81b28c631e53d9
Daniel Barclay <barclaysd@me.com>
2019-05-27 23:03:37 +0100

    Server side authentication and owner check to facilitate new team & channel creation. Client side check to prevent rendering of add channels and invite people

--- server/graphql/resolvers/channel.js
+++ server/graphql/resolvers/channel.js
@@ -1,23 +1,41 @@
 import formatErrors from '../../helpers/formatErrors';
+import { requiresAuth } from '../../auth/permissions';
 
 export default {
   Query: {},
   Mutation: {
-    createChannel: async (parent, args, { models }) => {
-      try {
-        const channel = await models.Channel.create(args);
-        return {
-          ok: true,
-          channel,
-        };
-      } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(err);
-        return {
-          ok: false,
-          errors: formatErrors(err, models),
-        };
-      }
-    },
+    createChannel: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          const team = await models.Team.findOne(
+            { where: { id: args.teamId } },
+            { raw: true },
+          );
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: 'name',
+                  message: 'Only the owner of a team can create channels',
+                },
+              ],
+            };
+          }
+          const channel = await models.Channel.create(args);
+          return {
+            ok: true,
+            channel,
+          };
+        } catch (err) {
+          // eslint-disable-next-line no-console
+          console.log(err);
+          return {
+            ok: false,
+            errors: formatErrors(err, models),
+          };
+        }
+      },
+    ),
   },
 };
