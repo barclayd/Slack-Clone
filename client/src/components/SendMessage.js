5cf275654b002c7b87fe04fd3f3717a19a7d5822
Daniel Barclay <barclaysd@me.com>
2019-05-28 00:03:30 +0100

    createMessageMutation to send messages to channels onKeyDown. Handled with formik

--- client/src/components/SendMessage.js
+++ client/src/components/SendMessage.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import styled from 'styled-components';
+import { withFormik } from 'formik';
+import { graphql, compose } from 'react-apollo';
 import { Input } from 'semantic-ui-react';
+import { createMessageMutation } from '../graphql/message';
 
 const SendMessageWrapper = styled.div`
   grid-column: 3;
   grid-row: 3;
-  margin: 15px;
+  padding: 20px;
 `;
 
-export default ({ channelName }) => (
+const ENTER_KEY = 13;
+
+const SendMessage = ({
+  channelName,
+  values,
+  handleBlur,
+  isSubmitting,
+  handleChange,
+  handleSubmit,
+}) => (
   <SendMessageWrapper>
-    <Input fluid placeholder={`Message #${channelName}`} />
+    <Input
+      name="message"
+      onBlur={handleBlur}
+      onChange={handleChange}
+      value={values.message}
+      fluid
+      placeholder={`Message #${channelName}`}
+      onKeyDown={(e) => {
+        if (e.keyCode === ENTER_KEY && !isSubmitting) {
+          handleSubmit(e);
+        }
+      }}
+    />
   </SendMessageWrapper>
 );
+
+export default compose(
+  graphql(createMessageMutation),
+  withFormik({
+    mapPropsToValues: () => ({ message: '' }),
+    handleSubmit: async (
+      values,
+      { props: { channelId, mutate }, resetForm },
+    ) => {
+      if (!values.message || !values.message.trim()) {
+        resetForm(false);
+        return;
+      }
+      const {
+        data: { createMessage },
+      } = await mutate({
+        variables: {
+          channelId,
+          text: values.message,
+        },
+      });
+      if (createMessage) {
+        resetForm();
+      }
+    },
+  }),
+)(SendMessage);
