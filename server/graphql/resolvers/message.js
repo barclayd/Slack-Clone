9018ac502ee0ae2462009fdee8681b78946497ac
Daniel Barclay <barclaysd@me.com>
2019-05-28 16:45:47 +0100

    MessageContainer to display a thread of messages for a specific channel with meta data.

--- server/graphql/resolvers/message.js
+++ server/graphql/resolvers/message.js
@@ -1,16 +1,29 @@
+import { requiresAuth } from '../../auth/permissions';
+
 export default {
+  Message: {
+    // eslint-disable-next-line max-len
+    user: ({ userId }, args, { models }) => models.User.findOne({ where: { id: userId } }, { raw: true }),
+  },
   Query: {
-    messages: async (parent, args, { models, user }) => [],
+    messages: requiresAuth.createResolver(
+      // eslint-disable-next-line max-len
+      async (parent, { channelId }, { models }) => models.Message.findAll({ where: { channelId } }, { raw: true }),
+    ),
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (err) {
-        console.log(err);
-        return false;
-      }
-    },
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({
+            ...args,
+            userId: user.id,
+          });
+          return true;
+        } catch (err) {
+          return false;
+        }
+      },
+    ),
   },
 };
